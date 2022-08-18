113c83d5a1e3b2d309aaa13e24ff7137bfd50c26
p1rt-py <103097530+p1rt-py@users.noreply.github.com>
2022-08-18 16:04:12 +0300

    Delete reviews directory

--- reviews/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Review, Comment, Title, Genre, Category
-
-# Register your models here.
-admin.site.register(Review)
-admin.site.register(Comment)
-admin.site.register(Title)
-admin.site.register(Genre)
-admin.site.register(Category)
